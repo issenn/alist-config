0a71b237efb2a95708b7472e9d73ccf450d37bf4
issenn <28648060+issenn@users.noreply.github.com>
2022-01-19 06:12:07 +0800

    .

--- alist.sh
+++ alist.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env sh
 
 # 创建配置文件目录
-mkdir data
+mkdir -p /opt/alist/data/
 
-cat > data/config.json <<EOF
+cat > /opt/alist/data/config.json <<EOF
 {
   "address": "0.0.0.0",
   "port": ${PORT},
-  "local": false,
+  "assets": "${ASSETS}",
   "database": {
-    "type": "${DATABASE}",
+    "type": "${DATABASE_TYPE}",
     "user": "${DATABASE_USER}",
     "password": "${DATABASE_PASSWORD}",
     "host": "${DATABASE_HOST}",
     "port": ${DATABASE_PORT},
     "name": "${DATABASE_NAME}",
     "table_prefix": "x_",
-    "db_file": "data/data.db"
+    "db_file": "/opt/alist/data/data.db"
+  },
+  "scheme": {
+    "https": false,
+    "cert_file": "",
+    "key_file": ""
+  },
+  "cache": {
+    "expiration": ${CACHE_EXPIRATION},
+    "cleanup_interval": ${CLEANUP_INTERVAL}
   }
 }
 EOF
 
+cd /opt/alist
 ./alist -conf data/config.json
